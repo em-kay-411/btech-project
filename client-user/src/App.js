6ffe1392b664dd6b91aa554a590124a80551a7d9
Mohit Kumbhare <emkay@Mohits-MacBook-Air.local>
2024-03-08 21:53:07 +0530

    Things wworking for phone

--- client-user/src/App.js
+++ client-user/src/App.js
@@ -8,58 +8,80 @@ function App() {
   const [swipeDirection, setSwipeDirection] = useState(true);
   const [formPosition, setFormPosition] = useState(0);
   const [mapPosition, setMapPosition] = useState(0);
+  const [touchStartX, setTouchStartX] = useState(0);
   const app = useRef(null);
 
-  const handleMouseMove = (event) => {
-    event.preventDefault();
-    const deltaX = event.deltaX;
-    // console.log(deltaX);
-
-    if(swipeDirection && deltaX > 0){
+  const handleDelta = (deltaX) => {
+    if (swipeDirection && deltaX > 0) {
       setFormPosition(formPosition - (deltaX * 0.08));
-      
-      if(Math.abs(formPosition) > 5){
+
+      if (Math.abs(formPosition) > 5) {
         setIsVisible(false);
         setTimeout(() => {
           setSwipeDirection(false);
         }, 1000)
       }
-      else{
+      else {
         setTimeout(() => {
-          setFormPosition(0);          
+          setFormPosition(0);
         }, 1000)
       }
     }
-    
-    if(!swipeDirection && deltaX < 0){
+
+    if (!swipeDirection && deltaX < 0) {
       setMapPosition(mapPosition - (deltaX * 0.08));
 
-      if(Math.abs(mapPosition) > 5){
+      if (Math.abs(mapPosition) > 5) {
         setIsVisible(true);
         setTimeout(() => {
           setSwipeDirection(true);
         }, 1000)
       }
-      else{
+      else {
         setTimeout(() => {
-          setMapPosition(0);          
+          setMapPosition(0);
         }, 1000)
       }
     }
   }
 
+  const handleMouseMove = (event) => {
+    event.preventDefault();
+    const deltaX = event.deltaX;
+    // console.log(deltaX);
+    handleDelta(deltaX);
+  }
+
+  const handleTouchMove = (event) => {
+    event.preventDefault();
+    const touch = event.touches[0];
+    const deltaX = touchStartX - touch.pageX;
+    console.log(touchStartX);
+    console.log(touch.pageX);
+
+    handleDelta(deltaX);
+  }
+
+  const handleTouchStart = (event) => {
+    setTouchStartX(event.touches[0].pageX);
+  }
+
   useEffect(() => {
     app.current.addEventListener('wheel', handleMouseMove);
+    app.current.addEventListener('touchstart', handleTouchStart);
+    app.current.addEventListener('touchmove', handleTouchMove);
 
     return () => {
       app.current.removeEventListener('wheel', handleMouseMove);
+      app.current.removeEventListener('touchstart', handleTouchStart);
+      app.current.removeEventListener('touchmove', handleTouchMove);
     }
   })
 
   return (
     <div className="App" ref={app}>
-      <Form visibility={isVisible} positionX={formPosition}/>
-      <Map visibility={!isVisible} positionX={mapPosition}/>
+      <Form visibility={isVisible} positionX={formPosition} />
+      <Map visibility={!isVisible} positionX={mapPosition} />
     </div>
   );
 }
