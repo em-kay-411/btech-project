7ed2771bc088e50aa3f9884fcc6e340e78aecfe4
Mohit Kumbhare <emkay@Mohits-MacBook-Air.local>
2024-03-18 12:04:27 +0530

    Detailed option card working but need to work a lot more

--- client-user/src/components/DetailedOptionCard.js
+++ client-user/src/components/DetailedOptionCard.js
@@ -1,63 +1,105 @@
+import Spinner from './Spinner';
 import '../css/DetailedOptionCard.css'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 import env from 'react-dotenv';
+const backendURL = env.BACKEND_API_URL;
 
-function DetailedOptionCard (props) {
-    const option = props.option;
-    const backendURL = env.BACKEND_API_URL;
+function DetailedOptionCard(props) {
+    const [option, setOption] = useState(props.option);
+    const [loading, setLoading] = useState(true);
     const [transitDetailsForBus, setTransitDetailsForBus] = useState([]);
 
-    const getNextStation = async (bus) => {
-        const routeArray = bus.route;
+    useEffect(() => {
+        const getNextStation = async (bus) => {
+            const routeArray = bus.route;
 
-        let i = 0;
-        while (i < routeArray.length) {
-            if (!routeArray[i].crossed) {
-                break;
+            let i = 0;
+            while (i < routeArray.length) {
+                if (!routeArray[i].crossed) {
+                    break;
+                }
+                i++;
             }
-        }
 
-        const stationID = routeArray[i].station;
-        const requestBody = {
-            stationID: stationID
-        }
-
-        const response = await axios.post(`${backendURL}stationNameFromID`, requestBody);
-        return response.data.stationName;
-    }
-
-    const getPrevStation = async (bus) => {
-        const routeArray = bus.route;
-
-        let i = 0;
-        while (i < routeArray.length) {
-            if (!routeArray[i].crossed) {
-                break;
+            if(i >= routeArray.length){
+                return 'journey end';
             }
-        }
 
-        i--;
-        if (i < 0) {
-            return 'began';
-        }
-        const stationID = routeArray[i].station;
-        const requestBody = {
-            stationID: stationID
-        }
-
-        const response = await axios.post(`${backendURL}stationNameFromID`, requestBody);
-        return response.data.stationName;
-    }
-
-    const handleTransitDetailsClick = async (bus) => {
-        const stationNames = bus.route.map(async (station) => {
+            const stationID = routeArray[i].station;
             const requestBody = {
-                stationID: station
+                stationID: stationID
             }
+
             const response = await axios.post(`${backendURL}stationNameFromID`, requestBody);
             return response.data.stationName;
-        })
+        }
+
+        const getPrevStation = async (bus) => {
+            const routeArray = bus.route;
+
+            let i = 0;
+            while (i < routeArray.length) {
+                if (!routeArray[i].crossed) {
+                    break;
+                }
+                i++;
+            }
+
+            i--;
+            if (i < 0) {
+                return 'began';
+            }
+            const stationID = routeArray[i].station;
+            const requestBody = {
+                stationID: stationID
+            }
+
+            const response = await axios.post(`${backendURL}stationNameFromID`, requestBody);
+            // console.log(response.data.stationName);
+            return response.data.stationName;
+        }
+
+        const fetchAllData = async () => {
+            setLoading(true);
+            const updatedOptions = await Promise.all(option.map(async (transit) => {
+                const buses = transit.buses;
+    
+                const updatedBuses = await Promise.all(buses.map(async (bus) => {
+                    const prevStation = await getPrevStation(bus);
+                    const nextStation = await getNextStation(bus);
+    
+                    return {
+                        ...bus, 
+                        prevStation : prevStation,
+                        nextStation : nextStation
+                    }
+                }));
+    
+                return {
+                    ...transit,
+                    buses: updatedBuses
+                };
+            }))
+
+            setOption(updatedOptions);
+            setLoading(false);
+        }        
+
+        fetchAllData();
+        console.log('useeffect', option);
+    }, [])
+
+    const handleTransitDetailsClick = async (bus) => {
+        const stationPromises = bus.route.map(async (station) => {
+            const requestBody = {
+                stationID: station
+            };
+            const response = await axios.post(`${backendURL}stationNameFromID`, requestBody);
+            return response.data.stationName;
+        });
+
+        const stationNames = await Promise.all(stationPromises);
 
         let i = 0;
         while (stationNames[i] !== bus.source) {
@@ -73,30 +115,34 @@ function DetailedOptionCard (props) {
         if (i < stationNames.length) {
             ans[stationNames[i]] = bus.route[i].crossed;
         }
-
         setTransitDetailsForBus(ans);
-        console.log(transitDetailsForBus);
     }
 
-    return (
+    return(
         <>
             <div className="detailed-option-card">
-                {option.map((transit) => {
+                {loading && <Spinner/>}
+                {!loading && option.map((transit) => {
+                    console.log(transit);
+                    const buses = transit.buses;
+                    console.log(buses);
                     return (
-                        <div className="transit-heading" key={`${transit.source}-${transit.destination}`}>
+                        <div className="transit-heading">
                             {transit.source} to {transit.destination}
                             <div className="transit-buses">
-                                {transit.buses.map((bus) => {
-                                    const prevStationPromise = getPrevStation(bus);
-                                    const nextStationPromise = getNextStation(bus);
-    
-                                    return(
-                                        <div className="bus" key={bus.id} onClick={() => handleTransitDetailsClick(bus)}>
-                                            <div className="bus-number-detailed-option-card">{bus.id}</div>
-                                            <div className="prev-station-detailed-option-card">Crossed {prevStationPromise}</div>
-                                            <div className="next-station-detailed-option-card">Arriving at {nextStationPromise} in (Calculate ETA)</div>
-                                        </div>
-                                    );
+                                {buses.map((bus) => {
+                                    try {
+                                        return (
+                                            <div className="bus" key={bus.id} onClick={() => handleTransitDetailsClick(bus)}>
+                                                <div className="bus-number-detailed-option-card">{bus.id}</div>
+                                                <div className="prev-station-detailed-option-card">{bus.prevStation === 'began' ? ('Began journey') : (`Crossed ${bus.prevStation}`)}</div>
+                                                <div className="next-station-detailed-option-card">Arriving at {bus.nextStation === 'journey end' ? ('end of the jounrey') : bus.nextStation} in (Calculate ETA)</div>
+                                            </div>
+                                        );
+                                    } catch (err) {
+                                        return null;
+                                    }
+
                                 })}
                             </div>
                         </div>
@@ -104,11 +150,11 @@ function DetailedOptionCard (props) {
                 })}
             </div>
             {transitDetailsForBus.length > 1 && <div className="transit-details-for-bus">
-    
+                Nothing
             </div>}
         </>
     );
-    
+
 }
 
 export default DetailedOptionCard;
