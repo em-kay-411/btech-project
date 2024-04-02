b4fd96896aa2729d1f1a476722320298f3a2ccca
Mohit Kumbhare <emkay@Mohits-MacBook-Air.local>
2024-04-02 09:59:16 +0530

    Added MQTT connection from user to bus

--- client-user/public/env.js
+++ client-user/public/env.js
@@ -66,5 +66,6 @@ window.env = {
   "npm_node_execpath": "/Users/emkay/.nvm/versions/node/v19.8.1/bin/node",
   "npm_config_prefix": "/Users/emkay/.nvm/versions/node/v19.8.1",
   "COLORTERM": "truecolor",
-  "BACKEND_API_URL": "http://192.168.0.103:3050/"
+  "BACKEND_API_URL": "http://192.168.0.103:3050/",
+  "MQTT_BROKER_URL": "ws://192.168.0.103:9001"
 };
--- client-user/src/components/OptionCard.js
+++ client-user/src/components/OptionCard.js
@@ -1,13 +1,33 @@
 import '../css/OptionCard.css'
+import {useEffect} from 'react';
 import DirectionsBusIcon from '@mui/icons-material/DirectionsBus';
 import GpsFixedTwoToneIcon from '@mui/icons-material/GpsFixedTwoTone';
 import LocationOnTwoToneIcon from '@mui/icons-material/LocationOnTwoTone';
+import mqtt from 'mqtt';
+import env from 'react-dotenv';
+const brokerURL = env.MQTT_BROKER_URL;
+const client = mqtt.connect(brokerURL);
 
 
 function OptionCard(props) {
     const stations = props.buses;
     const numOfChanges = props.numOfChanges;
     const onClick = props.onClick;
+
+    useEffect(() => {
+        const handleMessage = (topic, message) => {
+            const bus = topic.split('/')[1];
+            const location = JSON.parse(message);
+
+            console.log(`Location of ${bus} : ${location.latitude}, ${location.longitude}`);
+        }
+
+        client.on('message', handleMessage);
+
+        return () => {
+            client.off('message', handleMessage);
+        }
+    })
 
     return (
         <div className="option-card" onClick={onClick}>
@@ -16,8 +36,12 @@ function OptionCard(props) {
                 {(numOfChanges - 1 > 0) && (<div className="changes-count">{numOfChanges - 1} bus(s) to change </div>)}
             </div>
             {stations.map((transit, index) => {
-                {/* console.log(transit.buses); */}
                 const uniqueBuses = new Set(transit.buses.map(element => element.id))
+                uniqueBuses.forEach(bus => {
+                    client.subscribe(`location/${bus}`, () => {
+                        console.log(`subscribed to bus location from ${bus}`);
+                    });
+                })
                 return (
                     <div key={index} className="transit-card">
                         <div className="buses-to-display">{Array.from(uniqueBuses).map((busNumber, i) => {                             
