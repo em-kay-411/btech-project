ad5f1125082d677e5336de8891f9d02489be7ac2
Mohit Kumbhare <emkay@Mohits-MacBook-Air.local>
2024-04-12 16:42:08 +0530

    Added latitude longitude to database

--- database/prep/addLatitudeLongitude.js
+++ database/prep/addLatitudeLongitude.js
@@ -2,12 +2,34 @@ const fs = require('fs');
 const mongoose = require('mongoose');
 const Station = require('../models/station');
 const axios = require('axios');
+const { encode } = require('punycode');
 
 const main = async () => {
     await mongoose.connect('mongodb://localhost:27017/btech-project', {
         useNewUrlParser: true,
         useUnifiedTopology: true,
     });
+
+    // const encodedName = encodeURIComponent(`Shivajinagar Bus, Pune`);
+    // console.log(encodedName);
+    // const apiEndpoint = `https://api.tomtom.com/search/2/geocode/${encodedName}.json?key=YwnGgYME2e9Yhc5cENrbjM5NyRibrscM`;
+    // const response = await axios.get(apiEndpoint);
+
+    // const possiblePositions = response.data.results;
+
+    // let position;
+    // let i = 0;
+
+    // while (i < possiblePositions.length) {
+    //     if (possiblePositions[i].position.lat >= 18 && possiblePositions[i].position.lat < 19) {
+    //         position = possiblePositions[i].position;
+    //     }
+    //     i++;
+    // }
+    // console.log('Position', position);
+    // const station = await Station.updateOne({ name: 'Shivajinagar' }, { $set: { latitude: position.lat, longitude: position.lon } });
+    // console.log(station);
+    // console.log('Station location updated successfully for ', 'Shivajinagar');
 
     fs.readFile('stations.txt', 'utf8', (err, data) => {
         const stationNames = data.split('\n');
@@ -17,13 +39,22 @@ const main = async () => {
             try {
                 const stationObj = await Station.findOne({name : name});
                 if(!stationObj.latitude && !stationObj.longitude){
-                    const encodedName = encodeURIComponent(`${name} Bus Stand, Pune`);
+                    const encodedName = encodeURIComponent(`${name} Bus, Pune`);
                     const apiEndpoint = `https://api.tomtom.com/search/2/geocode/${encodedName}.json?key=YwnGgYME2e9Yhc5cENrbjM5NyRibrscM`;
                     const response = await axios.get(apiEndpoint);
 
-                    const position = response.data.results[0].position;
-                    console.log(position);
-                    
+                    const possiblePositions = response.data.results;
+
+                    let position;
+                    let i =0;
+
+                    while(i < possiblePositions.length){
+                        if(possiblePositions[i].position.lat >= 18 && possiblePositions[i].position.lat < 19){
+                            position = possiblePositions[i].position;
+                        }
+                        i++;
+                    }
+                    console.log('Position', position);
                     const station = await Station.updateOne({name : name}, {$set : {latitude : position.lat, longitude : position.lon}});
                     console.log(station);
                     console.log('Station location updated successfully for ', name);
@@ -32,7 +63,7 @@ const main = async () => {
                     console.log('already present');
                     count++;
                 }
-                
+
             } catch (error) {
                 console.error(error.message);
             }
@@ -40,7 +71,8 @@ const main = async () => {
 
         console.log('Already present ', count);
     })
-    
+
+    return 0;
 }
 
 main();
