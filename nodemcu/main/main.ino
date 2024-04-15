22bb0b6c71896d8cfb07fd703ebcc6ea5d65484d
Mohit Kumbhare <emkay@Mohits-MacBook-Air.local>
2024-04-15 14:03:01 +0530

    Testing to get bus route at the start of the journey

--- nodemcu/main/main.ino
+++ nodemcu/main/main.ino
@@ -1,34 +1,39 @@
 #include <TinyGPS++.h>
 #include <SoftwareSerial.h>
 #include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
 #include "PubSubClient.h"
 #include <ArduinoJson.h>
-TinyGPSPlus gps;          // The TinyGPS++ object
-SoftwareSerial ss(4, 5);  // The serial connection to the GPS device
-const char* busID = "1";
-const char* clientID = "1";
-const char* ssid = "Galaxy M3194C2";
-const char* password = "eeyy6643";
-const char* mqtt_server = "192.168.179.186";
-
-  WiFiClient wifiClient;
+TinyGPSPlus gps;         // The TinyGPS++ object
+SoftwareSerial ss(4, 5); // The serial connection to the GPS device
+const char *busID = "1";
+const char *clientID = "1";
+const char *ssid = "Galaxy M3194C2";
+const char *password = "eeyy6643";
+const char *server = "192.168.179.186";
+const char *backendPort = "3050";
+const char *MAPS_API_KEY = "YwnGgYME2e9Yhc5cENrbjM5NyRibrscM";
+const char *busRouteEndpoint = "http://" + server + ":" + backendPort + "/busRoute";
+WiFiClient wifiClient;
 
 // 1883 is the listener port for the Broker
-PubSubClient client(mqtt_server, 1883, wifiClient);
+PubSubClient client(server, 1883, wifiClient);
 
 float latitude, longitude;
 int year, month, date, hour, minute, second;
 String date_str, time_str, lat_str, lng_str;
 int pm;
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
   ss.begin(9600);
   Serial.println();
   Serial.print("Connecting to ");
   Serial.println(ssid);
   WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(500);
     Serial.print(".");
   }
@@ -36,18 +41,35 @@ void setup() {
   Serial.println("WiFi connected");
   // Print the IP address
   Serial.println(WiFi.localIP());
-  if (client.connect(clientID)) {
+  if (client.connect(clientID))
+  {
     Serial.println("Connected to MQTT Broker !");
-  } else {
+  }
+  else
+  {
     Serial.println("Connection to MQTT Broker failed…");
+  }
+
+  String requestBody = "{\"bus\" : \"" + busID + "\""; 
+
+  HTTPclient http;
+  Serial.println("Htting to ", busRouteEndpoint);
+  http.begin(busRouteEndpoint);
+  http.addHeader("Content-Type", "application/json");
+  int httpResponseCode = http.POST(requestBody);
+
+  if(httpResponseCode > 0){
+    Serial.println(http.getString());
   }
 }
 
-
-void loop() {
+void loop()
+{
   while (ss.available() > 0)
-    if (gps.encode(ss.read())) {
-      if (gps.location.isValid()) {
+    if (gps.encode(ss.read()))
+    {
+      if (gps.location.isValid())
+      {
         latitude = gps.location.lat();
         lat_str = String(latitude, 6);
         longitude = gps.location.lng();
@@ -66,13 +88,16 @@ void loop() {
         Serial.println(jsonString);
 
         // Publish the stringified JSON
-        if (client.publish("location/1", jsonString.c_str())) {
+        if (client.publish("location/1", jsonString.c_str()))
+        {
           Serial.println("Location sent!");
-        } else {
+        }
+        else
+        {
           Serial.println("Temperature failed to send.Reconnecting to MQTT Broker and trying again");
           client.connect(clientID);
-          delay(10);  // This delay ensures that client.publish doesn’t clash with the client.connect call
-         client.publish("location/1", jsonString.c_str());
+          delay(10); // This delay ensures that client.publish doesn’t clash with the client.connect call
+          client.publish("location/1", jsonString.c_str());
         }
       }
     }
