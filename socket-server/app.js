bfecef6ab8189d4bf9d95329f1a98337df73b7f3
Mohit Kumbhare <kumbharemohit10@gmail.com>
2024-04-30 19:51:02 +0530

    Changed port

--- socket-server/app.js
+++ socket-server/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const socketIO = require('socket.io');
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5500;
 
 const server = http.createServer(app);
 
